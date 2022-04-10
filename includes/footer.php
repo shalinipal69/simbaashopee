d1a64df5dbb13b3020b5e18a6731bcd55ee17138
shalini <ssshal1999@gmail.com>
2022-04-10 16:03:56 +0530

    simbaashopee

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+
+
+
+
+
+        <footer class="foot">
+           <div class="container">
+               <center>
+                   <p>Copyright <small>&copy;</small> Simbaa Shopee | All Rights Reserved.</p>
+               </center>
+           </div>
+           
+           
+       </footer>   
+        
+        
+        
+       
